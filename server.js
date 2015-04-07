64208fb64fd2f7ce801b3b01bff673817d9c41fb
David M Gudeman <davidmgudeman@gmail.com>
2015-04-06 18:05:45 -0700

    added a.html to fix the update problem

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 var express = require('express')
-  , app = express.createServer(express.logger())
+  , app = express()
   , pg = require('pg')
   , connectionString = process.env.DATABASE_URL || 'postgres://postgres:Printer238@localhost:5433/JPerL'
   , start = new Date()
-  , port = 80
-  , client;
+  , port = 3000
+  , client
+  , bodyParser = require('body-parser');
+
 
 client = new pg.Client(connectionString);
 client.connect();
-var flash = require('express-flash');
+//var flash = require('express-flash');
 
-app.configure(function(){
-    app.use(flash());
-    app.use(express.cookieParser('keyboard cat'));
-    app.use(express.session({ cookie: { maxAge: 60000 }, secret: 'topsecret'}));
-    app.use(express.bodyParser());
-    app.use(app.router);
-    app.use(express.compress());
+//app.configure(function(){
+  //  app.use(flash());
+   // app.use(express.cookieParser('keyboard cat'));
+   // app.use(express.session({ cookie: { maxAge: 60000 }, secret: 'topsecret'}));
+    app.use(bodyParser.urlencoded({extended: false}));
+   // app.use(app.router);
+   // app.use(express.compress());
     app.use(express.static(__dirname + '/JPerL4View/public'));    
-});
+//});
 
-app.get('/api/song', function(req, res) {
-  console.log("req: " + JSON.stringify(req.query));
-  var title = req.query.title;
-  var location = req.query.location;
-  var format = req.query.format;
-  var notes = req.query.notes;
-  var artist = req.query.artist;
-  var genre = req.query.genre;
-
+app.post('/api/song', function(req, res) {
+  console.log("req: " + JSON.stringify(req.body));
+  var title = req.body.title;
+  var location = req.body.location;
+  var format = req.body.format;
+  var notes = req.body.notes;
+  var artist = req.body.artist;
+  var genre = req.body.genre;
+  var songs = [];
   client.query('INSERT INTO songs(title, location, format, notes, artist, genre) VALUES($1, $2, $3, $4, $5, $6)', [title, location, format, notes, artist, genre]);
   
   query = client.query('SELECT * FROM songs');
@@ -38,10 +40,14 @@ app.get('/api/song', function(req, res) {
     if (!result) {
      // return res.send('No data found');
     } else {
-     // res.send('Song: ' + result.title);
+      songs.push({title: result.title, artist: result.artist});
     }
   }); 
-req.flash('info', 'song inserted');
+  query.on( 'end', function() {
+  res.send(songs);
+    client.end();
+});
+//req.flash('info', 'song inserted');
 //res.send("inserted song");
 });
 
@@ -65,7 +71,7 @@ app.get('/api/songs', function(req, res) {
      songString += (result.title + "<br />");
     }
   }); 
-req.flash('info', 'song inserted');
+//req.flash('info', 'song inserted');
 query.on( 'end', function() {
   res.send(songString);
     client.end();
